daec2052a399f4b9a492d8eb3732bc3e2b02aea4
marcusjwhelan <marcus.j.whelan@gmail.com>
2019-05-20 14:56:21 -0500

    basic v4 items used with swift 5

--- Package.swift
+++ Package.swift
@@ -8,12 +8,16 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0")
+        
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "0.1.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
